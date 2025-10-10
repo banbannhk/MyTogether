4d0b8370ec0b9544cae4dbd0ac498df8fabebeb9
Hein Ko <heinkolatt@codigo.co>
2025-10-10 18:11:42 +0700

    docker file setup

---
+++ src/main/java/org/th/MyTogetherApplication.java
@@ -0,0 +1,12 @@
+package org.th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTogetherApplication.class, args);
+    }
+}
